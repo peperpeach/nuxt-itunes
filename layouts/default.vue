04231c8e4f06ea500d24135a52ab49b812c776bd
yada <layada@bigc.co.th>
2019-04-24 14:24:02 +0700

    Init Nuxt.js iTunes Project

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <Toolbar />
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+import Toolbar from '@/components/Toolbar.vue';
+
+export default {
+  components: {
+    Toolbar,
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 md6 offset-md3>
+        <h1>Search iTunes</h1>
+        <br />
+        <v-form @submit.prevent="submit">
+          <v-text-field
+            placeholder="Enter Artist Name"
+            v-model="search"
+            autofocus
+          ></v-text-field>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      search: '',
+    };
+  },
+  methods: {
+    submit(event) {
+      this.$router.push(`results/${this.search}`);
+    },
+  }
+};
+</script>
